9c3a223c9f7192ef57b5456852b70c581af6587d
Uddeshya Tiwari <uddeshyatiwari9085@gmail.com>
2023-09-20 18:39:27 +0530

    fix

--- components/shared/Button.tsx
+++ components/shared/Button.tsx
@@ -15,14 +15,14 @@ const Button = ({ children, type, className, disabled, loading }: IButton) => {
   return type === ButtonType.SECONDARY ? (
     <button
       disabled={disabled}
-      className="bg-themeBlack flex items-center gap-2 border-2 border-themeBlue transition-all duration-700 rounded-lg px-8 min-h-[2.5rem] font-semibold text-lg"
+      className={`bg-themeBlack flex items-center gap-2 border-2 border-themeBlue transition-all duration-700 rounded-lg px-8 min-h-[2.5rem] font-semibold text-lg ${className}`}
     >
       {children} {loading && <CgSpinner className="animate-spin" />}
     </button>
   ) : (
     <button
       disabled={disabled}
-      className="bg-gradient-to-r flex items-center gap-2 from-themePurple to-themeBlue hover:from-themeBlue hover:to-themeBlue disabled:!from-themeGrey disabled:!to-themeGrey disabled:text-themeTextGrey transition-all duration-700 rounded-lg px-8 min-h-[2.5rem] font-semibold text-lg"
+      className={`bg-gradient-to-r flex items-center gap-2 from-themePurple to-themeBlue hover:from-themeBlue hover:to-themeBlue disabled:!from-themeGrey disabled:!to-themeGrey disabled:text-themeTextGrey transition-all duration-700 rounded-lg px-8 min-h-[2.5rem] font-semibold text-lg ${className}`}
     >
       {children} {loading && <CgSpinner className="animate-spin" />}
     </button>
