2029e7bece66f0f2b73063f0aa5fc92f9b63b52a
luffylovesmeat <uddeshyatiwari9085@gmail.com>
2024-01-18 14:44:28 +0530

    chat

--- app/(ai)/CG-AI/chat/[chatType]/[chatId]/page.tsx
+++ app/(ai)/CG-AI/chat/[chatType]/[chatId]/page.tsx
@@ -10,5 +10,13 @@ export default function AiChatPage({
     chatId: string
   }
 }) {
-  return <div>{params.chatType}</div>
+  return (
+    <div className="fixedHeight flex flex-col overflow-hidden px-4 py-4 md:py-8">
+      <div className="felx items-center justify-between">
+        <div className="text-3xl font-semibold capitalize md:text-5xl">
+          {params.chatType.split('-').join(' ')}
+        </div>
+      </div>
+    </div>
+  )
 }
